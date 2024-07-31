267b0f8e1f6d9441819feebcfbd1d117c1e81d77
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-07-31 21:30:08 +0300

    add some function and creat some more aria

--- snake.cpp
+++ snake.cpp
@@ -18,10 +18,20 @@
 Color blue = { 151 ,255 ,255 ,255 };
 Color green = { 0, 0, 0, 255 };
 
-int noktaboyutu = 30;
-int noktsayimi = 25;
+static int noktaboyutu = 30;
+static int noktsayimi = 25;
+static int offset = 75;
 
 double lastUpdateTime = 0;
+
+static bool ElementInDeque(Vector2 element, std::deque<Vector2> deque) {
+    for (unsigned int i = 0; i < deque.size(); i++) {
+        if (Vector2Equals(deque[i], element)) {
+            return true;
+        }
+    }
+    return false;
+}
 
 static bool eventTriggered(double interval) {
     double  currentTime = GetTime();
@@ -36,19 +46,31 @@ class Snake {
 public:
     std::deque<Vector2> body = { Vector2{6,9}, Vector2{5,9}, Vector2{4,9} };
     Vector2 direction = { 1,0 };
+    bool addSegment = false;
+
     void Draw() {
         for (int i = 0; i < body.size(); i++) {
             float x = body[i].x;
             float y = body[i].y;
-            Rectangle segment = Rectangle{x * noktaboyutu, y * noktaboyutu, float(noktaboyutu), float(noktaboyutu) };
+            Rectangle segment = Rectangle{offset + x * noktaboyutu, offset + y * noktaboyutu, float(noktaboyutu), float(noktaboyutu) };
             DrawRectangleRounded(segment,0.5,6,green);
         }
     }
     void Update() {
-        body.pop_back();
-        body.push_front(Vector2Add(body[0],direction));
+        body.push_front(Vector2Add(body[0], direction));
+        if (addSegment == true) {
+           
+            addSegment = false;
 
+        }
+        else {
+            body.pop_back();
+        }
+    }
 
+    void Reset() {
+        body = { Vector2{6,9}, Vector2{5,9}, Vector2{4,9} };
+        direction = { 1,0 };
     }
 };
 class Elma {
@@ -56,11 +78,11 @@ public:
     Vector2 position = {4 ,6};
     Texture2D texture;
 
-    Elma() {
+    Elma(std::deque<Vector2> snakeBody) {
         Image image = LoadImage("graph/gomb1.png");  // yapamadım
         texture = LoadTextureFromImage(image);
         UnloadImage(image);
-        position = GenerateRandomPos();
+        position = GenerateRandomPos(snakeBody);
     }
 
     ~Elma() {
@@ -68,13 +90,21 @@ public:
     }
 
     void Draw() {
-       DrawTexture(texture, position.x * noktaboyutu, position.y * noktaboyutu, WHITE);
+       DrawTexture(texture, offset + position.x * noktaboyutu, offset + position.y * noktaboyutu, WHITE);
     }
 
-    Vector2 GenerateRandomPos() {
+    Vector2 GenerateRandomhucre() {
         float x = GetRandomValue(0, noktsayimi - 1);
         float y = GetRandomValue(0, noktsayimi - 1);
-        return Vector2{x, y};
+        return Vector2{ x,y };
+    }
+    Vector2 GenerateRandomPos(std::deque<Vector2> snakeBody) {
+       
+        Vector2 position = GenerateRandomhucre();
+        while (ElementInDeque(position, snakeBody)) {
+            position = GenerateRandomhucre();
+        }
+        return position;
     }
      
 };
@@ -82,7 +112,8 @@ class Game {
 
 public:
     Snake snake = Snake();
-    Elma elma = Elma();
+    Elma elma = Elma(snake.body);
+    bool running = true;
 
     void Draw() {
         elma.Draw();
@@ -91,11 +122,45 @@ public:
 
     void Update() {
         snake.Update();
+        CheckCollisionWithElma();
+        CheckCollisionWithEdges();
+        CheckCollisionWithTail();
     }
 
+    void CheckCollisionWithElma() {
+        if (Vector2Equals(snake.body[0], elma.position)) {
+            elma.position = elma.GenerateRandomPos(snake.body);
+            snake.addSegment = true;
+        }
+    }
 
+    void CheckCollisionWithEdges() {
+        if (snake.body[0].x == noktaboyutu || snake.body[0].x == -1) {
+            GameOver();
+        }
+        if (snake.body[0].y == noktaboyutu || snake.body[0].y == -1) {
+            GameOver();
+        }
+    }
 
+    void GameOver() {
+        std::cout << "game over" << std::endl;
+        snake.Reset();
+        elma.position = elma.GenerateRandomPos(snake.body);
+        running = false;
+        //exit(1);
+    }
+    
 
+    void CheckCollisionWithTail() {
+        std::deque<Vector2> headlessBody = snake.body;
+        headlessBody.pop_front();
+        if (ElementInDeque(snake.body[0], headlessBody)) {
+            GameOver();
+        }
+        
+    
+    }
 
 };
 
@@ -104,7 +169,7 @@ public:
 
 
 int main(){
-    InitWindow(noktaboyutu*noktsayimi, noktaboyutu * noktsayimi, "THE SNAKE");
+    InitWindow(2* offset + noktaboyutu * noktsayimi, 2 * offset + noktaboyutu * noktsayimi, "THE SNAKE");
     SetTargetFPS(120);
 
     Game game;
@@ -117,19 +182,24 @@ int main(){
         }
         if (IsKeyPressed(KEY_UP) && game.snake.direction.y != 1) {
             game.snake.direction = { 0, -1 };
+            game.running = true;
         }
         if (IsKeyPressed(KEY_DOWN) && game.snake.direction.y != -1) {
             game.snake.direction = { 0, 1 };
+            game.running = true;
         }
         if (IsKeyPressed(KEY_LEFT) && game.snake.direction.x != 1) {
             game.snake.direction = { -1, 0 };
+            game.running = true;
         }
         if (IsKeyPressed(KEY_RIGHT) && game.snake.direction.x != -1) {
             game.snake.direction = { 1, 0 };
+            game.running = true;
         }
 
         //arka plan
         ClearBackground(blue);
+        DrawRectangleLinesEx(Rectangle{(float)offset-5,(float)offset-5,(float)noktaboyutu* noktsayimi+10,(float)noktaboyutu * noktsayimi + 10 }, 5, green);
         game.Draw();
         EndDrawing();
     }
